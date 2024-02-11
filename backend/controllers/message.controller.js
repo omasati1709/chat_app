3b87d8c191d305aaec4b2b0936b6b45726e65f5e
OM ASATI <asatiom1513@gmail.com>
2024-02-11 18:06:48 +0530

    added

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
 	try {
@@ -31,13 +32,18 @@ export const sendMessage = async (req, res) => {
 		// await newMessage.save();
 		await Promise.all([conversation.save(), newMessage.save()]);
 
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
 		res.status(201).json(newMessage);
 	} catch (error) {
 		console.log("Error in sendMessage controller: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
-
 
 export const getMessages = async (req, res) => {
 	try {
@@ -47,7 +53,6 @@ export const getMessages = async (req, res) => {
 		const conversation = await Conversation.findOne({
 			participants: { $all: [senderId, userToChatId] },
 		}).populate("messages"); 
-
 		if (!conversation) return res.status(200).json([]);
 
 		const messages = conversation.messages;
@@ -58,5 +63,3 @@ export const getMessages = async (req, res) => {
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
-
-
