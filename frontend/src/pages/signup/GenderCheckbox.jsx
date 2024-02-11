b91550f74a155a83d63c8463d133e21bacbc28e0
OM ASATI <asatiom1513@gmail.com>
2024-02-11 07:44:21 +0530

    code added

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,22 +1,29 @@
-// import React from 'react'
-
-export const GenderCheckbox = () => {
-  return (
-    <div className='flex'>
-        <div className='form-control'>
-            <label className={`label gap-2 cursor-pointer`}>
-                <span className='label-text'>Male</span>
-                <input type='checkbox' className='checkbox checkbox-md' />
-            </label>
-        </div>
-        <div className='form-control'>
-            <label className={`label gap-2 cursor-pointer`}>
-                <span className='label-text'>Female</span>
-                <input type='checkbox' className='checkbox checkbox-md ' />
-            </label>
-        </div>
-    </div>
-  )
-}
-
-
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+	return (
+		<div className='flex'>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
+					<span className='label-text'>Male</span>
+					<input
+						type='checkbox'
+						className='checkbox checkbox-md'
+						checked={selectedGender === "male"}
+						onChange={() => onCheckboxChange("male")}
+					/>
+				</label>
+			</div>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
+					<span className='label-text'>Female</span>
+					<input
+						type='checkbox'
+						className='checkbox checkbox-md'
+						checked={selectedGender === "female"}
+						onChange={() => onCheckboxChange("female")}
+					/>
+				</label>
+			</div>
+		</div>
+	);
+};
+export default GenderCheckbox;
